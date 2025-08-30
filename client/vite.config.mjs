15103418f1a49be33c853e735d791fffc945b0fc
shreya-krj <shreya.jags123@gmail.com>
2025-08-30 20:20:49 +0530

    Update and rename vite.config.js to vite.config.mjs

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -1,11 +1,9 @@
+// vite.config.mjs
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:5000",  // forward all /api calls to backend
-    },
-  },
 });
+
