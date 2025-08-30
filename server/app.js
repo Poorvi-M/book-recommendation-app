f754936253810b62ee60592a543d43e1e09d73d4
shreya-krj <shreya.jags123@gmail.com>
2025-08-30 20:08:07 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -5,7 +5,10 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5174",  // React dev server
+  credentials: true
+}));
 app.use(express.json());
 
 // Import routes
